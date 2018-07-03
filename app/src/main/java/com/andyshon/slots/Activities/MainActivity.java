4db2909fa2c44ecb313bb5bb76c997bde03166f1
Andyshon <mystatcontrol@gmail.com>
2018-07-04 00:39:02 +0300

    add slow mode for slots rotation, formatted jackpot text

--- app/src/main/java/com/andyshon/slots/Activities/MainActivity.java
+++ app/src/main/java/com/andyshon/slots/Activities/MainActivity.java
@@ -10,10 +10,12 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.Switch;
 
 import com.andyshon.slots.Adapter.ListAdapter;
 import com.andyshon.slots.CustomViews.CustomBetHighButton;
@@ -25,6 +27,7 @@ import com.andyshon.slots.R;
 
 import java.util.Collections;
 import java.util.List;
+
 
 public class MainActivity extends BaseActivity implements CustomBetHighButton.mButtonListener, CustomBetLowButton.mButtonListener, CustomSpinButton.mButtonListener,
                                                                 CustomSettingsButton.mButtonListener {
@@ -47,8 +50,10 @@ public class MainActivity extends BaseActivity implements CustomBetHighButton.mB
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        GlobalConstants.loadUserPreferences(this);
         initUI();
-        setAdapters();
+
+        setAdapters(true);
     }
 
 
@@ -88,19 +93,16 @@ public class MainActivity extends BaseActivity implements CustomBetHighButton.mB
         * */
 
         list1.setOnTouchListener(new View.OnTouchListener() {
-
             public boolean onTouch(View v, MotionEvent event) {
                 return (event.getAction() == MotionEvent.ACTION_MOVE);
             }
         });
         list2.setOnTouchListener(new View.OnTouchListener() {
-
             public boolean onTouch(View v, MotionEvent event) {
                 return (event.getAction() == MotionEvent.ACTION_MOVE);
             }
         });
         list3.setOnTouchListener(new View.OnTouchListener() {
-
             public boolean onTouch(View v, MotionEvent event) {
                 return (event.getAction() == MotionEvent.ACTION_MOVE);
             }
@@ -109,40 +111,51 @@ public class MainActivity extends BaseActivity implements CustomBetHighButton.mB
 
 
     /*
-    * todo: Обнуляем позиции всех слотов, перетасовываем комбинации и обновляем слоты
+    * todo: Перетасовываем и устанавливаем комбинации адаптерам
     * */
 
-    private void setAdapters(){
-        list1.setSelection(0);
-        list2.setSelection(0);
-        list3.setSelection(0);
+    private void setAdapters(boolean isFastMode){
 
         GlobalConstants.shuffleMainLine();
 
+        if (isFastMode) {
+            setAdapterForList1();
+            setAdapterForList2();
+            setAdapterForList3();
+        }
+    }
 
+
+    /*
+    * todo: Установка адаптеров для listViews
+    * */
+
+    private void setAdapterForList1 () {
         Collections.shuffle(GlobalConstants.combinations);
 
-        GlobalConstants.combinations.set(GlobalConstants.combinations.size()-2, GlobalConstants.expectedCombination_1);
+        GlobalConstants.combinations.set(GlobalConstants.combinations.size() - 2, GlobalConstants.expectedCombination_1);
         List<Integer> l1 = GlobalConstants.getShuffledImages(GlobalConstants.combinations);
-        l1.set(GlobalConstants.combinations.size()-2, GlobalConstants.drawable_1);
+        l1.set(GlobalConstants.combinations.size() - 2, GlobalConstants.drawable_1);
         ListAdapter adapter1 = new ListAdapter(MainActivity.this, GlobalConstants.combinations, l1);
         list1.setAdapter(adapter1);
+    }
 
-
+    private void setAdapterForList2 () {
         Collections.shuffle(GlobalConstants.combinations);
 
-        GlobalConstants.combinations.set(GlobalConstants.combinations.size()-2, GlobalConstants.expectedCombination_2);
+        GlobalConstants.combinations.set(GlobalConstants.combinations.size() - 2, GlobalConstants.expectedCombination_2);
         List<Integer> l2 = GlobalConstants.getShuffledImages(GlobalConstants.combinations);
-        l2.set(GlobalConstants.combinations.size()-2, GlobalConstants.drawable_2);
-        ListAdapter adapter2 = new ListAdapter(MainActivity.this, GlobalConstants.combinations, l2);
+        l2.set(GlobalConstants.combinations.size() - 2, GlobalConstants.drawable_2);
+        final ListAdapter adapter2 = new ListAdapter(MainActivity.this, GlobalConstants.combinations, l2);
         list2.setAdapter(adapter2);
+    }
 
-
+    private void setAdapterForList3 () {
         Collections.shuffle(GlobalConstants.combinations);
 
-        GlobalConstants.combinations.set(GlobalConstants.combinations.size()-2, GlobalConstants.expectedCombination_3);
+        GlobalConstants.combinations.set(GlobalConstants.combinations.size() - 2, GlobalConstants.expectedCombination_3);
         List<Integer> l3 = GlobalConstants.getShuffledImages(GlobalConstants.combinations);
-        l3.set(GlobalConstants.combinations.size()-2, GlobalConstants.drawable_3);
+        l3.set(GlobalConstants.combinations.size() - 2, GlobalConstants.drawable_3);
         ListAdapter adapter3 = new ListAdapter(MainActivity.this, GlobalConstants.combinations, l3);
         list3.setAdapter(adapter3);
     }
@@ -170,8 +183,7 @@ public class MainActivity extends BaseActivity implements CustomBetHighButton.mB
 
         new CountDownTimer(1500, 10000) {
             @Override
-            public void onTick(long millisUntilFinished) {
-            }
+            public void onTick(long millisUntilFinished) {}
 
             @Override
             public void onFinish() {
@@ -193,6 +205,15 @@ public class MainActivity extends BaseActivity implements CustomBetHighButton.mB
         RelativeLayout MainLayout = (RelativeLayout) findViewById(R.id.MainLayout);
         LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
         View customView = inflater.inflate(R.layout.popup_settings, null);
+
+        Switch switch_mode_fast = customView.findViewById(R.id.switch_mode_fast);
+        switch_mode_fast.setChecked(GlobalConstants._IsFastMode);
+        switch_mode_fast.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                GlobalConstants._IsFastMode = b;
+            }
+        });
 
         mPopupWindow = new PopupWindow(customView, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT);
         if (Build.VERSION.SDK_INT >= 21) {
@@ -218,17 +239,26 @@ public class MainActivity extends BaseActivity implements CustomBetHighButton.mB
         else {
             GlobalConstants._UserCoins += prize - GlobalConstants._BetAmount;
             updateUserCoins();
+            mPopupWindow.dismiss();
+            background_trans.setVisibility(View.GONE);
             showWinnerPopUp(prize);
         }
     }
 
 
     /*
-    * todo: Форматируем текст и обновляем джэкпот (не может быть меньше 100,000)
+    * todo: Форматируем текст (запятая как разделитель) и обновляем джэкпот (всегда >= 100,000)
     * */
 
     private void updateJackpot() {
-        btnJackpot.setText(String.valueOf(GlobalConstants._Jackpot));
+
+        String text = String.valueOf(GlobalConstants._Jackpot);
+        String p1, p2;
+        p1 = text.substring(0, 3);
+        p2 = text.substring(3, text.length());
+        text = p1 + "," + p2;
+
+        btnJackpot.setText(text);
     }
 
 
@@ -277,16 +307,13 @@ public class MainActivity extends BaseActivity implements CustomBetHighButton.mB
 
 
     /*
-    * todo: Перетасовываем и плавно вращаем слоты
+    * todo: Вращаем слоты в быстром режиме
     * */
 
-    @Override
-    public void onSpin() {
-        updateRat();
-        customSpinButton.setEnabled(false);
-        setAdapters();
+    private void SpinFast () {
 
         final Handler handler = new Handler();
+
         final Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -337,11 +364,149 @@ public class MainActivity extends BaseActivity implements CustomBetHighButton.mB
 
 
     /*
+    * todo: Вращаем слоты по очереди (в медленном режиме). Три потока (слота) начинают по очереди вращаться.
+    * todo: Четвертый поток ждёт пока третий слот закончит вращаться и вызывает метод givePrizeToPlayer()
+    * */
+
+    private void SpinSlow () {
+
+        final Handler handler = new Handler();
+
+        final Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int listViewSize = list1.getAdapter().getCount();
+
+                for (int index = 0; index < listViewSize; index++) {
+                    list3.smoothScrollToPositionFromTop(list2.getLastVisiblePosition() + 100, 0, 500);
+                    try {
+                        Thread.sleep(60);
+                    } catch (InterruptedException e) {
+                    }
+                }
+                // todo: для плавного выравнивания слотов
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        list3.smoothScrollToPosition(GlobalConstants.combinations.size()-1);
+                    }
+                });
+            }
+        });
+
+        final Thread thread4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    thread3.join();
+
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            givePrizeToPlayer();
+                        }
+                    }, 500);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        final Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int listViewSize = list1.getAdapter().getCount();
+
+                for (int index = 0; index < listViewSize; index++) {
+                    list2.smoothScrollToPositionFromTop(list2.getLastVisiblePosition() + 100, 0, 500);
+                    try {
+                        Thread.sleep(60);
+                    } catch (InterruptedException e) {
+                    }
+                }
+                // todo: для плавного выравнивания слотов
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        list2.smoothScrollToPosition(GlobalConstants.combinations.size()-1);
+
+                        setAdapterForList3();
+
+                        thread3.start();
+                        thread4.start();
+                    }
+                });
+            }
+        });
+
+        final Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int listViewSize = list1.getAdapter().getCount();
+
+                for (int index = 0; index < listViewSize; index++) {
+                    list1.smoothScrollToPositionFromTop(list1.getLastVisiblePosition() + 100, 0, 500);
+                    try {
+                        Thread.sleep(60);
+                    } catch (InterruptedException e) {
+                    }
+                }
+                // todo: для плавного выравнивания слотов
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        list1.smoothScrollToPosition(GlobalConstants.combinations.size()-1);
+
+                        setAdapterForList2();
+
+                        thread2.start();
+                    }
+                });
+            }
+        });
+
+        setAdapterForList1();
+
+        thread1.start();
+    }
+
+
+    /*
+    * todo: Перетасовываем и плавно вращаем слоты
+    * */
+
+    @Override
+    public void onSpin() {
+        updateRat();
+        customSpinButton.setEnabled(false);
+        setAdapters(GlobalConstants._IsFastMode);
+
+        if (GlobalConstants._IsFastMode)
+            SpinFast();
+        else
+            SpinSlow();
+    }
+
+
+    /*
     * todo: Показываем всплывающее окно настроек
     * */
 
     @Override
     public void onShowSettings() {
         showPopUpSettings();
+    }
+
+
+    /*
+    * todo: При уходе из активити сохранить данные игрока
+    * */
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        GlobalConstants.saveUserPreferences(this);
     }
 }
