341a20f2648161e32a0251a00e28e2d2d0b11fc4
Andyshon <mystatcontrol@gmail.com>
2018-07-04 14:30:50 +0300

    update

--- app/src/main/java/com/andyshon/slots/Adapter/ListAdapter.java
+++ app/src/main/java/com/andyshon/slots/Adapter/ListAdapter.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.andyshon.slots.R;
 
@@ -16,17 +15,15 @@ import java.util.List;
  * Created by andyshon on 03.07.18.
  */
 
-public class ListAdapter extends ArrayAdapter<String> {
+public class ListAdapter extends ArrayAdapter<Integer> {
 
     private final Activity context;
-    private final List<String> combinations;
     private final List<Integer> images;
 
 
-    public ListAdapter(Activity context, List<String> combinations, List<Integer> images) {
-        super(context, R.layout.list_single, combinations);
+    public ListAdapter(Activity context, List<Integer> images) {
+        super(context, R.layout.list_single, images);
         this.context = context;
-        this.combinations = combinations;
         this.images = images;
     }
 
@@ -37,12 +34,9 @@ public class ListAdapter extends ArrayAdapter<String> {
         if (rowView == null) {
             rowView = inflater.inflate(R.layout.list_single, null, true);
         }
-        TextView txtTitle = (TextView) rowView.findViewById(R.id.combination);
-
         ImageView imageView = (ImageView) rowView.findViewById(R.id.img);
-        txtTitle.setText(combinations.get(position));
-
         imageView.setImageResource(images.get(position));
+
         return rowView;
     }
 
